539d6c12fd70d0eca616050a5cf511716d4ee9e0
Abhishek Rajput <124500096+AbhishekRajputweb@users.noreply.github.com>
2023-04-11 22:05:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+
+var a="7",b="8",c="9",d="4",e="5",f="6",g="1",h="2",i="3",j="0",l="*",m="/",n="-",o="+",z;
+
+        function a7()
+        {
+            
+            document.getElementById('screen1').value+=a;
+        }
+        function a8()
+        {
+            
+            document.getElementById('screen1').value+=b;
+        }
+        function a9()
+        {
+            
+            document.getElementById('screen1').value+=c;
+        }
+        function a4()
+        {
+            
+            document.getElementById('screen1').value+=d;
+        }
+        function a5()
+        {
+            
+            document.getElementById('screen1').value+=e;
+        }
+        function a6()
+        {
+            
+            document.getElementById('screen1').value+=f;
+        }
+        function a1()
+        {
+            
+            document.getElementById('screen1').value+=g;
+        }
+        function a2()
+        {
+            
+            document.getElementById('screen1').value+=h;
+        }
+        function a3()
+        {
+            
+            document.getElementById('screen1').value+=i;
+        }
+        function a0()
+        {
+            
+            document.getElementById('screen1').value+=j;
+        }
+    
+
+        function mul()
+        {
+            
+            document.getElementById('screen1').value+=l;
+        }
+        function diy()
+        {
+            
+            document.getElementById('screen1').value+=m;
+        }
+        function adi()
+        {
+            
+            document.getElementById('screen1').value+=n;
+        }
+        function sub()
+        {
+            
+            document.getElementById('screen1').value+=o;
+        }
+        
+
+        function br()
+             {
+                z=eval(document.getElementById('screen1').value);
+            document.getElementById('res').value=z;
+             }
+        
+        function clearinput()
+        {
+            document.getElementById('screen1').value="";
+            document.getElementById('res').value="";
+        }
+    
